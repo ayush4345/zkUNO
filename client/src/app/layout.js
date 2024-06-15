2cf5b0dc2f7fe7310a0a007a111471b4e1782e84
saurabh <sauraj9290@gmail.com>
2024-06-15 11:30:23 +0530

    add background image

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`bg-cover bg-[url("/bg.png")] ${inter.className}`}>{children}</body>
     </html>
   );
 }
--- client/src/components/homePage/Hero.jsx
+++ client/src/components/homePage/Hero.jsx
@@ -5,7 +5,7 @@ import Spline from '@splinetool/react-spline';
 
 const Hero = () => (
   <section className={`${styles.yPaddings} `}>
-    <Spline scene="https://prod.spline.design/PP5UHnFUA0YPE-z0/scene.splinecode" />
+    {/* <Spline scene="" /> */}
   </section>
 );
 
